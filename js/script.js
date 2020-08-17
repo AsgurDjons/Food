60771426a69b93838ee38b31f978de3010ab978c
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-08-17 17:02:58 +0300

    POST GET
    server  JSON и дд

--- js/script.js
+++ js/script.js
@@ -92,14 +92,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const btns = document.querySelectorAll('[data-modal]'),
           exit = document.querySelector('[data-close]'),
           modal = document.querySelector('.modal'),
-          openModal = setTimeout(showModal, 3000);
+          openModal = setTimeout(showModal, 5000);
 
     function showModal () {
         modal.style = 'display: block;';
         document.body.style.overflow = 'hidden'; 
         clearInterval(openModal);
     }
-    function noneModal () {
+    function hiddenModal () {
         modal.style = 'display: none;';
         document.body.style.overflow = '';  
     }
@@ -120,17 +120,17 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     exit.addEventListener('click', (e) => {
         if (e.target) {
-            noneModal (); 
+            hiddenModal (); 
         }
     });
     modal.addEventListener('click', (e) => {
         if (e.target == modal) {
-            noneModal (); 
+            hiddenModal (); 
         }
     });
     document.addEventListener('keydown', (e) => {
         if (e.code == 'Escape'&& (modal.style = 'display: block;')) {
-            noneModal ();
+            hiddenModal ();
         }
         
     });
@@ -204,4 +204,53 @@ window.addEventListener('DOMContentLoaded', () => {
         '.menu .container'
         ).render();
 
+        // Forms
+
+        const forms = document.querySelectorAll('form');
+        const message = {
+            loading: 'Загрузка...',
+            success: 'Спасибо! Скоро мы с вами свяжемся',
+            failure: 'Что-то пошло не так...'
+        };
+    
+        forms.forEach(item => {
+            postData(item);
+        });
+    
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
